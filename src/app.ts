97a93e15eb5b02b3aeb89b4ce4eda27474385a65
Binh21112004 <binh21112004vn@gmail.com>
2026-03-13 14:23:33 +0700

    video30

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+import express from "express";
+
+const app = express();
+const PORT = 8080;
+
+app.get("/", (req,res) => {
+  res.send("Hello World nodemon");
+})
+
+
+app.get("/hoidanit", (req,res) => {
+  res.send("Hello Eric");
+})
+
+
+app.listen(PORT, () => {
+  console.log(`My app is running on port: ${PORT} `)
+})
