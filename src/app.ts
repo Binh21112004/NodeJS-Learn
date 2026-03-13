ca18074f800f2f9915d18c5c82d69d0db197eade
Binh21112004 <binh21112004vn@gmail.com>
2026-03-13 16:33:12 +0700

    video 36

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,19 @@
 import express from "express";
-
+import 'dotenv/config';
+import webRoutes from "./routes/web";
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
-app.get("/", (req,res) => {
-  res.send("Hello World nodemon");
-})
+// config view engine
+app.set('view engine','ejs');
+app.set('views', __dirname + '/views' );
 
+//config routes
+webRoutes(app)
 
-app.get("/hoidanit", (req,res) => {
-  res.send("Hello Eric");
-})
-
-
+//config static files : images/css/js
+app.use(express.static('public'));
 app.listen(PORT, () => {
-  console.log(`My app is running on port: ${PORT} `)
+  console.log(`My app is running on port: ${PORT} `);
+  console.log("env port: ", process.env.PORT);
 })
